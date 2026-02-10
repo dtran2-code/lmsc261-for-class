cb4ab8da21337105d09d88085787f2184300d364
dao <dtran2@berklee.edu>
2026-02-10 18:23:42 -0500

    Submission

---
+++ Week-4/python_data_types.py
@@ -0,0 +1,8 @@
+integer_one = 1
+float_one = 1.0
+string_one = "1"
+
+# Just to check if its correct
+print(type(integer_one))
+print(type(float_one))
+print(type(string_one))
---
+++ notes.py
@@ -0,0 +1,28 @@
+# Data Types
+
+# Strings
+name = "Dao"
+last_name = "Tran"
+type(name)
+
+print(type(name))
+
+#Int(eger) - whole number
+age = 22
+x = 3
+y = 3
+print(x + y)
+
+
+# Flat - decimal point number
+pi = 3.14159265
+
+# Boolean (True or false)
+is_hungry = True
+print(age==pi)
+
+# Calling the function print
+print(is_hungry)
+
+#Exponentiation
+house_square_fottage = 1000**2
