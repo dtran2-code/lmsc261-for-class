d56c8a32864ad6f26a3ca59386722ca7696ee385
dao <dtran2@berklee.edu>
2026-02-25 16:19:57 -0500

    ask for float

---
+++ Week-5/ask_for_float.py
@@ -0,0 +1,4 @@
+answer = input("Arrghh I'm a troll, here is my riddle: Number 'tween numbers, what lies in the middle? ")
+float_answer = float(answer)
+answer_type = type(float_answer)
+print(float_answer, answer_type)
