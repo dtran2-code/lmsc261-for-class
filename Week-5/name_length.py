77ac4e71f19da3c6ac2918597d3da97399ce6fa8
dao <dtran2@berklee.edu>
2026-02-25 16:27:18 -0500

    length of name

---
+++ Week-5/name_length.py
@@ -0,0 +1,4 @@
+name_answer = input("ARGGH, I'm a troll, let's play a game: I'll let you pass, if you give me your -full- name!  ")
+length_of_name = len(name_answer)
+type_name = type(name_answer)
+print(length_of_name, type_name)
