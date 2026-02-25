a9a63f551937b8bd3c0b23d6695a977cfbab805f
dao <dtran2@berklee.edu>
2026-02-25 15:30:40 -0500

    arguments

---
+++ Week-5/arguments.py
@@ -0,0 +1,14 @@
+tenArguments = "oney, twoey, threeey, fourey, fivey, sixey, seveney, eightey, niney, teney"
+
+print()
+# 2. Nothing happens if there is no argument. There's nothing TO print.
+
+testingPower = pow(5)
+print(testingPower)
+# 3. Without the second argument, pow() also doesn't work. It needs to be exponentiated by SOMETHING
+
+# 4. round
+# 5. Defined
+# 6. As many arguments as I want. It'll round all of the arguments individually
+# 7. 3.14159 and 2
+# 8. It won't return anything since the function is not being called. If called, it will return 3 and 2, respectively
