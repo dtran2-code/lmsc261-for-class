6a95609d7737a16e3b6ddf6e4c99e902976082ea
dao <dtran2@berklee.edu>
2026-02-25 15:19:18 -0500

    function labels

---
+++ Week-5/function_labels.py
@@ -0,0 +1,89 @@
+# int() function
+# Example of calling the int() function
+
+numberer = int("45")
+print(numberer)
+
+# Name: integer, int()
+# Argument: any integer or "string" (still has to be an integer) in integer or string form
+# Side Effects: nothing
+# Return Value: Any integer the user typed in (as an integer or string) as an integer
+
+####################################################
+
+# str() function
+# Example of calling the str() function
+
+stringerer = str(45)
+print(stringerer)
+
+# Name: string, str()
+# Argument: anything, to be converted into a string
+# Side Effects: nothing
+# Return Value: Anything the user typed in as a string type
+
+####################################################
+
+# bool() function
+# Example of calling the bool() function
+
+booleaner = bool("Hello, World")
+print(booleaner)
+
+# Name: boolean, bool()
+# Argument: anything, to be returned as True or False
+# Side Effects: nothing
+# Return Value: True or False, depending on the argument
+
+####################################################
+
+# float() function
+# Example of calling the float() function
+
+floaterer = float(2.234)
+print(floaterer)
+
+# Name: float, float()
+# Argument: Any float, integer, or "string" (still an integer or a float), to be returned as True or False
+# Side Effects: nothing
+# Return Value: Any float, integer, or "string" (still has to  be an integer or float) as a float
+
+####################################################
+
+# len() function
+# Example of calling the len() function
+
+lengthener = len("Hello, World")
+print(lengthener)
+
+# Name: length, len()
+# Argument: anything, to be returned it's length
+# Side Effects: nothing
+# Return Value: the "length" of the argument, including spaces and punctuation
+
+####################################################
+
+# pow() function
+# Example of calling the pow() function
+
+powerer = pow(3,2)
+print(powerer)
+
+# Name: power, pow()
+# 1st Argument: any integer or float as the base | 2nd Argument: any integer or float as the exponent
+# Side Effects: nothing
+# Return Value: Exponentiates the base, calculates the argument
+
+####################################################
+
+# abs() function
+# Example of calling the abs() function
+
+absoluter = abs(-123.23)
+print(absoluter)
+
+# Name: absolute value, abs()
+# Argument: any int or float, negative or positive, to be returned the absolute value of that number
+# Side Effects: nothing
+# Return Value: the absolute value of the number in the argument
+
--- notes.py
+++ notes.py
@@ -1,5 +1,5 @@
 # Data Types
-
+"""
 # Strings
 name = "Dao"
 last_name = "Tran"
@@ -26,3 +26,77 @@ print(is_hungry)
 
 #Exponentiation
 house_square_fottage = 1000**2
+
+
+print() function
+
+Calling the print() function
+print("Hello, World!")
+
+Name: print
+Argument: whatever is in the parentheses of the function
+Side FX: Prints the arguments
+Return: None
+
+Type function
+
+type()
+
+Recalls the "type" of data... data type
+
+Name: type
+Argument: anything
+Side FX: there aren't any
+Return: the data type of the argument(s)
+
+Input function
+
+input()
+
+Name: input
+Arguments: Prompt for the user
+Side FX: Print's the prompt in the terminal and waits
+Return Value: Whatever the user typed in, as a string.
+
+Changing types
+
+users_age = input("What is your age?)
+users_age = int(users_age)
+
+Now, it would change the string int an integer
+You can also specify in the input function ưhat dât type í allowed
+
+users_age = int(input("What is your age?"))
+
+Length function
+
+len("Hello, World")
+Counts the number of letters and spaces in the string
+
+Name: len
+Argument: the thing you want the length of
+Side FX: none
+Return: the "length" of the argument
+
+Power function
+
+pow()
+pow(3,2)
+3^2
+
+Name: pow
+1st Argument: base, 2nd Argument: exponent
+Side FX: Nothing
+Return Value: the first argument to the power of the second argument
+
+**Return Value: the answer that is saved to a variable, usually to be printed in order to be seen
+
+Absolute value Function
+
+abs()
+abs(-123)
+
+Name: abs()
+Argument: the int you want the absolute value of
+Side FX: nothing
+Return Value: absolute value of the argument
